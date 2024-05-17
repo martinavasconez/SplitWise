6075e0f5e4a6eaabc8c78416430d8e5c7e559815
nitou2504 <sthefanou25@gmail.com>
2024-05-16 19:07:16 -0500

    refactor: Add details and user list to Grupo class

--- frontend/lib/Grupo.dart
+++ frontend/lib/Grupo.dart
@@ -1,18 +1,23 @@
 class Grupo {
   int id;
   String nombre;
+  String detalles;
   Map<int, Map<String, double>> articulos; // Mapa de usuario ID a artículos y costo
   double total;
   Map<int, double> totalPorUsuario; // Total gastado por cada usuario
   Map<int, Map<int, double>> deudaPorUsuario; // Deuda de cada usuario hacia otros usuarios
+  Map<int, String> listaDeUsuarios; // Mapa de usuario ID a nombre
+
 
   Grupo({
     required this.id,
     required this.nombre,
+    this.detalles = "",
     this.articulos = const {},
     this.total = 0,
     this.totalPorUsuario = const {},
     this.deudaPorUsuario = const {},
+    this.listaDeUsuarios = const {},
   });
 
   // Método para añadir un artículo al grupo
--- frontend/lib/Usuario.dart
+++ frontend/lib/Usuario.dart
@@ -12,6 +12,6 @@ class Usuario {
     this.nombre = "", // Inicialización con cadena vacía
     this.correo = "", // Inicialización con cadena vacía
     this.password = "", // Inicialización con cadena vacía
-    required this.listaDeGrupos,
-  });
+    this.listaDeGrupos = const [], // Inicialización con lista vacía constante
+    });
 }
