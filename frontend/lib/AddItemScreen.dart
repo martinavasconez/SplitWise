7e41da3fb320f87ad7ea50fb71fd876f6f870fb7
nitou2504 <sthefanou25@gmail.com>
2024-05-17 10:48:27 -0500

    add item logic

--- frontend/lib/AddItemScreen.dart
+++ frontend/lib/AddItemScreen.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/material.dart';
-import 'Grupo.dart';
+import 'GroupDetailsScreen.dart';
+import 'Grupo.dart'; // Make sure this import points to the correct location of Grupo.dart
 import 'Usuario.dart';
 
 class AddItemScreen extends StatefulWidget {
   final Usuario? usuario;
   final Grupo? group;
-  
 
-  AddItemScreen({this.group, this.usuario});
+  AddItemScreen({Key? key, this.usuario, this.group}) : super(key: key);
 
   @override
   _AddItemScreenState createState() => _AddItemScreenState();
 }
 
-class _AddItemScreenState extends State<AddItemScreen>
-    with SingleTickerProviderStateMixin {
+class _AddItemScreenState extends State<AddItemScreen> with SingleTickerProviderStateMixin {
   TabController? _tabController;
   final TextEditingController descriptionController = TextEditingController();
   final TextEditingController priceController = TextEditingController();
@@ -50,6 +49,7 @@ class _AddItemScreenState extends State<AddItemScreen>
       body: TabBarView(
         controller: _tabController,
         children: [
+          // Description tab content
           Padding(
             padding: EdgeInsets.all(16.0),
             child: Column(
@@ -57,10 +57,7 @@ class _AddItemScreenState extends State<AddItemScreen>
               children: [
                 Text(
                   'Description',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                  ),
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                 ),
                 SizedBox(height: 10),
                 TextField(
@@ -73,6 +70,7 @@ class _AddItemScreenState extends State<AddItemScreen>
               ],
             ),
           ),
+          // Price tab content
           Padding(
             padding: EdgeInsets.all(16.0),
             child: Column(
@@ -80,10 +78,7 @@ class _AddItemScreenState extends State<AddItemScreen>
               children: [
                 Text(
                   'Price',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                  ),
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                 ),
                 SizedBox(height: 10),
                 Container(
@@ -112,14 +107,28 @@ class _AddItemScreenState extends State<AddItemScreen>
                 SizedBox(
                   width: double.infinity,
                   child: ElevatedButton(
-                    onPressed: () {
-                      // Aquí puedes añadir la lógica para añadir el ítem
-                      Navigator.pop(context); // Regresa a la pantalla anterior (GroupDetailsScreen)
+                    onPressed: () async {
+                      // Assuming the user ID is available and passed correctly
+                      int? userId = widget.usuario?.id; // Make sure to handle nullability appropriately
+                      String itemName = descriptionController.text;
+                      double cost = double.parse(priceController.text);
+
+                      // Call addItem method
+                      await widget.group!.addItem(itemName, cost, userId!);
+
+                      // Call update methods
+                      await widget.group!.updateDeudaPorUsuario();
+                      await widget.group!.updateGroupDetails();
+
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => GroupDetailsScreen(group: widget.group, usuario: widget.usuario)));
+
+
+                      // Navigator.pop(context); // Navigate back after operations
                     },
                     child: Text('Add Item'),
                     style: ElevatedButton.styleFrom(
-                      backgroundColor: Color.fromARGB(255, 78, 179, 204), // Color del botón
-                      foregroundColor: Colors.white, // Color del texto
+                      backgroundColor: Color.fromARGB(255, 78, 179, 204),
+                      foregroundColor: Colors.white,
                       padding: EdgeInsets.symmetric(vertical: 16),
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(8.0),
@@ -132,6 +141,13 @@ class _AddItemScreenState extends State<AddItemScreen>
             ),
           ),
         ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // This will now be handled by the ElevatedButton in the Price tab
+        },
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
       ),
     );
   }
